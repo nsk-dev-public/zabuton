a6dd40a8597ea48ea43e4a98f5fd969f2529795a
n <>
2020-04-20 02:25:20 +0900

    init

---
+++ src/Model/Table/OdaiTable.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * 
+ * Odaiテーブル
+ *
+ * @since  20190708
+ *
+ */
+
+namespace App\Model\Table;
+
+use Cake\ORM\Table;
+
+class OdaiTable extends AppTable
+{
+
+	public function initialize(array $config)
+	{
+		// table名セット
+		$this->setTable('odai_t');
+	}
+
+
+}
